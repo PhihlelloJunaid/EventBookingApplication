5f1b55bfd4055e7ec596f526fa5af6d12ce1a5fc
Snap Tech <phihle15.com@gmail.com>
2025-06-24 15:53:53 +0200

    PJ MAROGA( Customer class)

--- src/main/java/za/ac/cput/domain/Customer.java
+++ src/main/java/za/ac/cput/domain/Customer.java
@@ -1,18 +1,18 @@
 //PHIHLELLO jUNAID MAROGA
 //STUDENT :219354359
-//Group : KN13
+//Group ; kN13
 package za.ac.cput.domain;
 
 public class Customer {
     private String address;
     private int contactNumber;
 
-    public Customer(){
+    private Customer(){
 
     }
-    public Customer (String address , int  contactNumber){
-        this.address = address;
-        this.contactNumber =contactNumber;
+    public Customer(Builder builder){
+        this.address = builder.address;
+        this.contactNumber = builder.contactNumber;
     }
     public String getAddress(){
         return address;
@@ -27,5 +27,28 @@ public class Customer {
                 "address='" + address + '\'' +
                 ", contactNumber=" + contactNumber +
                 '}';
+    }
+    public static class Builder {
+        private String address;
+        private int contactNumber;
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setContactNumber(int contactNumber) {
+            this.contactNumber = contactNumber;
+            return this;
+        }
+        public Builder copy(Customer customer){
+            this.address= customer.address;
+            this.contactNumber =customer.contactNumber;
+            return this;
+        }
+
+        public Customer build (){
+            return new Customer (this);
+        }
     }
 }
