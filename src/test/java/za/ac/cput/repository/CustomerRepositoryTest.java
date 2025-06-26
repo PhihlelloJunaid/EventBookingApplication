a13085e37c1a33c29804b4a493fa1d1f4c2d58f1
Snap Tech <phihle15.com@gmail.com>
2025-06-26 18:49:59 +0200

    Merge remote-tracking branch 'origin/master'

--- src/test/java/za/ac/cput/repository/CustomerRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/CustomerRepositoryTest.java
@@ -1,27 +1,54 @@
 package za.ac.cput.repository;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.factory.CustomerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CustomerRepositoryTest {
+    private iCustomerRepository repository = CustomerRepository.getRepository();
+    private Customer customer = CustomerFactory.createCustomer("10 Dorset",0711343);
 
     @Test
-    void create() {
+    @Order(1)
+    void a_create() {
+        Customer created  = repository.create(customer);
+        assertNotNull(created);
+        System.out.println("Created : " + created);
+    }
+    @Test
+    @Order(2)
+    void b_read() {
+        Customer read = repository.read(customer.);
+        assertNotNull(read);
+        assertEquals(customer.getContactNumber(),read.getAddress());
+        System.out.println("Read : " + read);
+
+
     }
 
     @Test
-    void read() {
+    @Order(3)
+    void c_update() {
+        Customer newCustomer =new Customer.Builder().copy(customer).setAddress("").build();
+        repository.update(newCustomer);
+        assertNotNull(repository.update(newCustomer));
+        System.out .println("Updated : " + newCustomer);
+
     }
 
     @Test
-    void update() {
+    @Order(4)
+    @Disabled
+    void d_delete() {
+        boolean success = repository.delete(customer.getContactNumber());
+        assertTrue(success);
+        System.out.println("Deletes : " + customer.getContactNumber());
     }
 
     @Test
-    void delete() {
-    }
-
-    @Test
-    void getAll() {
+    void e_getAll() {
+        System.out.println("All Customers : " + repository.getAll());
     }
 }
